64794c98443c8573edea184a2d2c9728fd28580b
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-04-22 12:18:06 -0400

    Adding Amirzaidi GoogleIcons
    Adding Amirzaidi GoogleIcons

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -9,11 +9,22 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-# Adding iamsj7/iPixel-Launcher as Prebuilt for now will ad as source in newer android versions
+# Adding iamsj7/iPixel-Launcher as Prebuilt for now will add as source in newer android versions if I can.
 include $(CLEAR_VARS)
 LOCAL_MODULE := PixelHome
 LOCAL_MODULE_OWNER := lineage
 LOCAL_SRC_FILES := apps/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
+
+# Adding amirzaidi/GoogleIcons as Prebuilt for now will add as source in newer android versions if I can.
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleIcons
+LOCAL_MODULE_OWNER := lineage
+LOCAL_SRC_FILES := apps/$(LOCAL_MODULE).apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_MODULE_CLASS := APPS

85325835bdf122de1859421eaa36109e6aebe897
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-05-14 22:56:37 -0400

    Adding OpenCamera, HackersKeyboard & iamsj7/iPixel-Launcher
    Adding OpenCamera, HackersKeyboard & iamsj7/iPixel-Launcher

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -9,20 +9,31 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-# Adding iamsj7/iPixel-Launcher as Prebuilt for now will add as source in newer android versions if I can.
+# Adding amirzaidi/GoogleIcons as Prebuilt for now will add as source in newer android versions if I can.
 include $(CLEAR_VARS)
-LOCAL_MODULE := PixelHome
+LOCAL_MODULE := GoogleIcons
 LOCAL_MODULE_OWNER := lineage
-LOCAL_SRC_FILES := apps/$(LOCAL_MODULE)/$(LOCAL_MODULE).apk
+LOCAL_SRC_FILES := apps/$(LOCAL_MODULE).apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
 
-# Adding amirzaidi/GoogleIcons as Prebuilt for now will add as source in newer android versions if I can.
+# Adding klausw/HackersKeyboard as Prebuilt for now will add as source in newer android versions if I can.
 include $(CLEAR_VARS)
-LOCAL_MODULE := GoogleIcons
+LOCAL_MODULE := HackersKeyboard
+LOCAL_MODULE_OWNER := lineage
+LOCAL_SRC_FILES := apps/$(LOCAL_MODULE).apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
+
+# Adding mark-h/OpenCamera as Prebuilt for now will add as source in newer android versions if I can.
+include $(CLEAR_VARS)
+LOCAL_MODULE := OpenCamera
 LOCAL_MODULE_OWNER := lineage
 LOCAL_SRC_FILES := apps/$(LOCAL_MODULE).apk
 LOCAL_MODULE_TAGS := optional
